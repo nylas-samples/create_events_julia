55606bad07af19cf34c52ec4bebaf39479f13f1c
Blag <atejada@gmail.com>
2023-02-14 05:22:31 -0500

    Create CreateEvent.jl
    Main source for creating events using Julia

---
+++ CreateEvent.jl
@@ -0,0 +1,30 @@
+# Load packages
+using DotEnv
+using HTTP
+using JSON
+
+# Load .env file
+cfg = DotEnv.config()
+
+# Events endpoint
+url = "https://api.nylas.com/events?notify_participants=true"
+
+# Body of the call
+payload = Dict("title" => "Learn Julia with Nylas",
+                      "when" => Dict("start_time" => 1675868400, "end_time" => 1675870200),
+                      "location" => "Blag\'s Den",
+                      "calendar_id" => cfg["CALENDAR_ID"],
+                      "participants" =>  [ Dict("email" => "atejada@gmail.com",
+                                                         "name" => "Blag")])
+
+# Headers for authorization
+headers = Dict(
+    "Accept" => "application/json",
+    "Content-Type"=> "application/json",
+    "Authorization"=> "Bearer " * cfg["ACCESS_TOKEN"]
+)
+
+# Calling the endpoint
+response = HTTP.post(url, headers, json(payload))
+# Print the response of the call
+print(response)
